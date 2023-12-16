0b22668ac63fec9a0533620d30b0d1267986fd35
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2023-12-16 16:41:16 +0530

    💝𝗤𝗨𝜩𝜩𝗡│𝜟𝗟𝗘𝗫𝗔│𝗠𝗗💝

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,16 +1,16 @@
 import fetch from 'node-fetch'
-import { youtubedl } from '@bochilteam/scraper'
+import { youtubedl } from '@bochilteam/scraper-sosmed'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	if (!args[0]) throw `Example: ${usedPrefix + command} https://youtu.be/S1--lhvwLsc`
 	if (!args[0].match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Invalid Youtube URL.`)
 	try {
 		let anu = await youtubedl(args[0])
-		m.react('🎶')
 		let data = anu.audio[Object.keys(anu.audio)[0]]
 		let url = await data.download()
 		if (data.fileSize > 400000) return m.reply(`Filesize: ${data.fileSizeH}\nUnable to send, maximum file size is 400 MB`)
 		await conn.sendFAudio(m.chat, { [/mp3/g.test(command) ? 'document' : 'audio']: { url: url }, mimetype: 'audio/mpeg', fileName: `${anu.title}.mp3` }, m, anu.title, anu.thumbnail, args[0])
+    m.react('🎤')
 	} catch (e) {
 		console.log(e)
 		try {
@@ -41,9 +41,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	}
 }
 
-handler.help = ['song <url>']
+handler.help = ['ytmp3 <url>']
 handler.tags = ['dl']
-handler.command = ['song', 'play'] 
-handler.diamond = false
+handler.command = ['ytmp3', 'yta'] 
 
 export default handler
